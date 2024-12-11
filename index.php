55aa74871118de0ee22ff792c5e217a4529da053
mmakhloufi2018 <38525392+mmakhloufi2018@users.noreply.github.com>
2024-12-11 01:15:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-
-echo "Hello World!";
+   // connect to mongodb
+   $m = new MongoClient();
+   echo "Connection to database successfully";
+	
+   // select a database
+   $db = $m->"simoweb1-database";
+   echo "Database mydb selected";
+   $collection = $db->mycol;
+   echo "Collection selected succsessfully";
+   $cursor = $collection->find();
+   // iterate cursor to display title of documents
+	
+   foreach ($cursor as $document) {
+      echo $document["title"] . "\n";
+   }
+?>
