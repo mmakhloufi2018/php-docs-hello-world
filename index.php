8a47c76816e6034ad93f514de3fc0c7cc16920e8
mmakhloufi2018 <38525392+mmakhloufi2018@users.noreply.github.com>
2024-12-11 01:36:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
 <?php
-require 'vendor/autoload.php'; // Inclure l'autoloader de Composer
+// Inclure l'autoloader généré par Composer
+require 'vendor/autoload.php';
 
 try {
+    // Paramètres de connexion à MongoDB Azure Cosmos DB
+    $host = "simoweb1-server.mongo.cosmos.azure.com";
+    $port = 10255;
+    $username = "simoweb1-server";
+    $password = "5bM7vQcnGVifk7kzIf5jpffqIYp1sAb8o0AySPBZOUkNzDOvY47dDJbjscnTvtlZ3qUXSna0LjtsACDbW7qKOQ==";
+    $dbname = "simoweb1-database";
+    $ssl = true;
+
+    // URI de connexion
+    $uri = sprintf(
+        "mongodb://%s:%s@%s:%d/%s?ssl=%s",
+        $username,
+        urlencode($password),
+        $host,
+        $port,
+        $dbname,
+        $ssl ? 'true' : 'false'
+    );
+
     // Connexion à MongoDB
-    $client = new MongoDB\Client("mongodb://<username>:<password>@<host>:<port>/<dbname>");
+    $client = new MongoDB\Client($uri);
     echo "Connection to database successfully<br>";
 
     // Sélection de la base de données
-    $db = $client->selectDatabase("simoweb1-database");
+    $db = $client->selectDatabase($dbname);
     echo "Database selected successfully<br>";
 
     // Sélection de la collection
